f9539cd5c18e7c5216043a86a47bf268485ddc5c
cjdupuis2 <43114361+cjdupuis2@users.noreply.github.com>
2020-12-18 16:47:13 -0600

    Update sine.m

--- sine.m
+++ sine.m
@@ -1,8 +1,9 @@
 %%
 clc; clear; close all;
 
-pixSize = 1024;
-wantedDist = 10;
+pixSize = 1024; 
+wantedDist = 10; %THE WANTED DISTANCE AND ALPHA ARE THE IMPORTANT VARIBABLES TO CHANGE
+alpha = 0.1;
 
 a = pixSize / wantedDist;
 freq = 2*pi / a;
@@ -11,7 +12,8 @@ lims  = [0 pixSize - 1];
 %phase = pi/2;
 %xp = y*sin(phase);
 ref = sin(freq*y);
-def = stretch(ref,0.2,1024);
+def = stretch(ref, alpha, pixSize);
+%def = clnhann(def); If you want, this is just hann filter on the deformed to make it cleaner. I wrote a function to make easier
 
 % figure; surf(ref), shading flat, colormap(gray), xlabel('X'), ylabel('Y'), zlabel('Z');
 % figure, imshow(ref,[]); xlabel('X'), ylabel('Y');
@@ -24,8 +26,20 @@ refFFTLog = fftshift(log(1+abs(refFFT)));
 defFFT = fft2(def);
 defFFTLog = fftshift(log(1+abs(defFFT)));
 
+a = 513; %All of these a's are just to plot the red points, you can comment out these and the plot3() lines to just see the graphs
+aP = a + wantedDist;
+aM = a - wantedDist;
+aPS = a + wantedDist * (1 - alpha);
+aMS = a - wantedDist * (1 - alpha);
+
 % figure, imshow(refFFTLog,[]); xlabel('X'), ylabel('Y');
 % figure, imshow(defFFTLog,[]); xlabel('X'), ylabel('Y');
- figure; surf(refFFTLog);shading flat; colormap(jet);  xlabel('X'), ylabel('Y'), zlabel('Z'); view(90,0);
- figure; surf(defFFTLog);shading flat; colormap(jet);  xlabel('X'), ylabel('Y'), zlabel('Z'); view(90,0);
+
+figure; hold; surf(refFFTLog);shading flat; colormap(jet);  xlabel('X'), ylabel('Y'), zlabel('Z'); view(90,0);
+plot3(a, aP, refFFTLog(aP, a), 'r*')
+plot3(a, aM, refFFTLog(aM, a), 'r*')
+ 
+figure; hold; surf(defFFTLog);shading flat; colormap(jet);  xlabel('X'), ylabel('Y'), zlabel('Z'); view(90,0);
+ plot3(a, aPS, defFFTLog(aPS, a), 'r*')
+ plot3(a, aMS, defFFTLog(aMS, a), 'r*')
 
